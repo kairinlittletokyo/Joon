82cd628ed9a01a8c2d2b18fd802428b00bbf5773
kairinlittletokyo <152365866+kairinlittletokyo@users.noreply.github.com>
2023-12-05 09:11:59 +0900

    [Bronze V] Title: A-B, Time: 208 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int a = 9, b = 1;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a - b);
+	}
+}
